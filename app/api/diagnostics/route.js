908ce39fba6f91f9452476dc41320af2554d36ad
James Perko <james.perko@me.com>
2026-08-24 16:43:42 -0400

    Fix diagnostics db import path

--- app/api/diagnostics/route.js
+++ app/api/diagnostics/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { loadToledo, calculateModel } from '../../../../lib/db';
+import { loadToledo, calculateModel } from '../../../lib/db';
 
 export const dynamic = 'force-dynamic';
 
